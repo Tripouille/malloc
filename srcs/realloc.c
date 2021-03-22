6406dbbc5aefc751602368be8b516bc375ab5b48
Tripouille <jmichel.gambard@gmail.com>
2021-03-22 19:56:46 +0100

    working on realloc (resize)

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -2,6 +2,9 @@
 
 void *
 realloc(void *ptr, size_t size) {
+	t_zone_header *		zone = NULL;
+	t_zone_header **	first_zone = NULL;
+
 	if (ptr == NULL)
 		return (malloc(size));
 	else if (!size)
@@ -9,8 +12,14 @@ realloc(void *ptr, size_t size) {
 		free(ptr);
 		return (NULL);
 	}
-	t_block_manager *ptr_manager = get_block_manager(ptr);
-	if (ptr_manager == NULL)
+	zone = get_ptr_zone(ptr, &first_zone);
+	if (zone == NULL)
 		return (NULL);
+	t_block_manager *block_manager = get_block_manager(ptr, zone);
+	if (block_manager == NULL)
+		return (NULL);
+	if (is_large_zone(zone) && zone->zone_size - sizeof(t_block_manager) >= size)
+		return (ptr);
+	//resize
 	return (NULL);
 }
