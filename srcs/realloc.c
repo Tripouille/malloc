404ac03e07bf750671909ad0cf096d9711eb661f
Tripouille <jmichel.gambard@gmail.com>
2021-03-27 13:26:14 -0400

    add split block in realloc

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -33,6 +33,15 @@ extend_memory(t_ptr_infos *infos, size_t size) {
 		return (move_into_new_block(infos, size));
 }
 
+static void
+split_block(t_ptr_infos * infos, size_t size) {
+	size_t	available_size = infos->block_manager->block_size - size - sizeof(t_block_manager);
+	infos->block_manager->block_size = size;
+	t_block_manager * new_block_manager = NEXT_BLOCK_MANAGER(infos->block_manager);
+	new_block_manager->block_size = available_size;
+	new_block_manager->is_free = 1;
+}
+
 void *
 realloc(void *ptr, size_t size) {
 	t_ptr_infos infos;
@@ -51,13 +60,7 @@ realloc(void *ptr, size_t size) {
 	if (infos.block_manager->block_size >= size)
 	{
 		if (infos.block_manager->block_size - size > sizeof(t_block_manager))
-		{
-			size_t	available_size = infos.block_manager->block_size - size - sizeof(t_block_manager);
-			infos.block_manager->block_size = size;
-			t_block_manager * new_block_manager = NEXT_BLOCK_MANAGER(infos.block_manager);
-			new_block_manager->block_size = available_size;
-			new_block_manager->is_free = 1;
-		}
+			split_block(&infos, size);
 		return (ptr);
 	}
 	else
