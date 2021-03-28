a0a388faa9499c8ceecd2ed2bcf68917ecbddf54
Tripouille <jmichel.gambard@gmail.com>
2021-03-28 16:35:54 -0400

    fix mem_ex

--- srcs/show_alloc_mem_ex.c
+++ srcs/show_alloc_mem_ex.c
@@ -15,21 +15,25 @@ show_zone_ex(t_zone_header *zone_header) {
 	size_t page_size = getpagesize();
 
 	for (t_zone_header * zone = zone_header; zone != NULL; zone = zone->next_zone_header) {
-		prev_page = 0;
-		used_page = 0;
+		prev_page = -1; used_page = 0;
 
 		prints("[");
 		for (t_block_manager * block_manager = ZONE_HEADER_SHIFT(zone);
 		ptr_is_in_zone(BLOCK_MANAGER_SHIFT(block_manager), zone);
 		block_manager = NEXT_BLOCK_MANAGER(block_manager)) {
-			actual_page = (size_t)block_manager / page_size;
-			print_block_ex(block_manager);
+
+			actual_page = (size_t)((void*)block_manager - ZONE_HEADER_SHIFT(zone)) / page_size;
+			print_block_ex(block_manager); set_color(RESET);
 			if (ptr_is_in_zone(NEXT_BLOCK_MANAGER(block_manager), zone))
 				prints(" ");
-			set_color(RESET);
-			if (actual_page != prev_page && !block_manager->is_free) {
+			size_t	block_end_page = (NEXT_BLOCK_MANAGER(block_manager) - ZONE_HEADER_SHIFT(zone) - 1) / page_size;
+			if (!block_manager->is_free && actual_page != prev_page) {
 				used_page += div_up(block_manager->block_size + sizeof(t_block_manager), page_size);
 				prev_page = actual_page;
+			}
+			else if (!block_manager->is_free && actual_page != block_end_page) {
+				used_page += div_up(block_manager->block_size + sizeof(t_block_manager), page_size);
+				prev_page = block_end_page;
 			}
 		}
 		size_t	total_page = (zone->zone_size + sizeof(t_zone_header)) / page_size;
--- tests/test2.c
+++ tests/test2.c
@@ -2,16 +2,22 @@
 
 int main(void)
 {
-	int i;
-	i = 0;
-	char * addr;
-	while (i < 1024)
+	//int i;
+	//i = 0;
+	char * addr; (void)addr;
+	addr = malloc(2032);
+		show_alloc_mem_ex();
+	addr = malloc(234234234);
+		show_alloc_mem_ex();
+	
+
+	/*while (i < 1024)
 	{
 		addr = (char*)malloc(1024);
 		addr[0] = 42;
 		//show_alloc_mem_ex();
 		free(addr);
 		i++;
-	}
+	}*/
 	return (0);
 }
