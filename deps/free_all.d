664db041fb2bf5df0a5ca7ca2e96bf54d3abb76b
Tripouille <jmichel.gambard@gmail.com>
2021-03-29 09:34:14 -0400

    clean all

--- deps/free_all.d
+++
@@ -1,5 +0,0 @@
-objs/free_all.o: srcs/free_all.c includes/ft_malloc.h \
- includes/memory_manager.h includes/utils.h
-includes/ft_malloc.h:
-includes/memory_manager.h:
-includes/utils.h:
--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -8,10 +8,9 @@
 
 void *		malloc(size_t size);
 void *		salloc(size_t size, char value);
-void 		*calloc(size_t nmemb, size_t size);
-void		free(void * ptr);
+void *		calloc(size_t nmemb, size_t size);
 void *		realloc(void *ptr, size_t size);
-void		free_all(void);
+void		free(void * ptr);
 
 void		show_alloc_mem(void);
 void		show_alloc_mem_ex(void);
